d45843f0d315c6ad9917f6dcb6c89e2e28c8c831
Akshay More <akshaymore7362@gmail.com>
2026-09-22 12:57:40 +0530

    increase navbar logo size

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -186,7 +186,7 @@ export function Header() {
           <img
             src="/brand-wordmark.svg"
             alt="The Success World"
-            style={{ width: "clamp(210px, 22vw, 290px)", height: "auto", objectFit: "contain", flexShrink: 0 }}
+            style={{ width: "clamp(250px, 26vw, 340px)", height: "auto", objectFit: "contain", flexShrink: 0 }}
           />
         </Link>
 
